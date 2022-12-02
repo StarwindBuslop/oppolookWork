eac1dc9a98d0dbdf351613b1e43766f43081a5a7
frbustlop@gmail.com <francisco.bustamante@solera.com>
2022-12-02 11:51:38 +0100

    Creating ALL client tests

--- demoX/src/test/java/com/testAssigment/demoX/testClient.java
+++ demoX/src/test/java/com/testAssigment/demoX/testClient.java
@@ -1,5 +1,25 @@
 package com.testAssigment.demoX;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class testClient {
-    
+    @Test
+    void testCreateClient(){
+        assertEquals(null,"contact added");
+    }
+    @Test
+    void testDeleteClient(){
+
+        assertEquals("opportunity is deleted","opportunity added");
+    }
+    @Test
+    void testListClients(){
+
+        assertEquals("opps are listed","contact added");
+    }
 }
